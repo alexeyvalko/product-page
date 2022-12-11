c6861097441e5fc0f863c03a47efd710ea34b35b
Aliaksei Valko <60567379+alexeyvalko@users.noreply.github.com>
2022-12-11 20:35:47 +0300

    fix: fix ARIA attributes

--- js/index.js
+++ js/index.js
@@ -12,8 +12,10 @@ class Accordion {
    * @param {HTMLElement} content - Content element.
    * @param {number} duration - The duration of animation in ms.
    * @param {string} easing - The rate of the animation's change over time.
+   * @param {string} id - Accordion id.
    */
-  constructor(details, content, duration, easing) {
+  constructor(details, content, duration, easing, id) {
+    this.id = id;
     this.easing = easing;
     this.duration = duration;
     this.details = details;
@@ -83,6 +85,18 @@ class Accordion {
     };
   }
 
+  #createIDs() {
+    this.details.setAttribute('id', `${this.id}-details`);
+    this.summary.setAttribute('id', `${this.id}-summary`);
+    this.content.setAttribute('id', `${this.id}-content`);
+  }
+
+  #removeIDs() {
+    this.details.removeAttribute('id');
+    this.summary.removeAttribute('id');
+    this.content.removeAttribute('id');
+  }
+
   #updateARIA() {
     this.summary.setAttribute('aria-expanded', this.details.open);
   }
@@ -90,13 +104,17 @@ class Accordion {
   #addARIAandRole() {
     this.summary.setAttribute('role', 'button');
     this.summary.setAttribute('aria-expanded', this.details.open);
-    this.summary.setAttribute('aria-controls', this.details.id);
+    this.summary.setAttribute('aria-controls', this.content.id);
+    this.content.setAttribute('role', `region`);
+    this.content.setAttribute('aria-labelledby', this.summary.id);
   }
 
   #removeARIAandRole() {
     this.summary.removeAttribute('role');
     this.summary.removeAttribute('aria-expanded');
     this.summary.removeAttribute('aria-controls');
+    this.content.removeAttribute('role');
+    this.content.removeAttribute('aria-labelledby');
   }
 
   /**
@@ -126,6 +144,7 @@ class Accordion {
    */
   attach() {
     this.summary.addEventListener('click', this.#clickHandler);
+    this.#createIDs();
     this.#addARIAandRole();
   }
 
@@ -135,21 +154,20 @@ class Accordion {
   detach() {
     this.summary.removeEventListener('click', this.#clickHandler);
     this.#removeARIAandRole();
+    this.#removeIDs();
   }
 }
 
-const addIdToElement = (element, idPrefix = 1) => {
-  if (!element.id) {
-    element.setAttribute('id', `accordion-${idPrefix}`);
-  }
-  return element;
+const generateUniqueId = (idPrefix, index = 1) => {
+  return `${idPrefix}-${index}`;
 };
 
 const generateMultipleAccordions = (
   detailsSelector,
   contentSelector,
   duration,
-  easing
+  easing,
+  idPrefix
 ) => {
   const detailsArray = Array.from(document.querySelectorAll(detailsSelector));
   if (!detailsArray.length)
@@ -158,8 +176,14 @@ const generateMultipleAccordions = (
     .map((details, index) => {
       const content = details.querySelector(contentSelector);
       if (content) {
-        const detailsWithId = addIdToElement(details, index + 1);
-        return new Accordion(detailsWithId, content, duration, easing);
+        const id = generateUniqueId(idPrefix, index + 1);
+        return new Accordion(
+          details,
+          content,
+          duration,
+          easing,
+          id
+        );
       }
     })
     .filter((accordion) => !!accordion);
@@ -172,12 +196,18 @@ const generateOneAccordion = (
   easing,
   idPrefix
 ) => {
-  const detailsElement = document.querySelector(detailsSelector);
-  if (!detailsElement) throw new Error(`Error: didn't find "details" tag.`);
+  const details = document.querySelector(detailsSelector);
+  if (!details) throw new Error(`Error: didn't find "details" tag.`);
   const content = detailsElement.querySelector(contentSelector);
   if (!content) throw new Error(`Error: didn't find content tag.`);
-  const detailsElementWithId = addIdToElement(detailsElement, idPrefix);
-  return new Accordion(detailsElementWithId, content, duration, easing);
+  const id = generateUniqueId(idPrefix);
+  return new Accordion(
+    details,
+    content,
+    duration,
+    easing,
+    id
+  );
 };
 
 /**
@@ -187,7 +217,7 @@ const generateOneAccordion = (
  * @param {string} options.contentSelector - The name of the "content" selector.
  * @param {string} [options.easing] - The rate of the animation's change over time.
  * @param {number} [options.duration] - The duration of animation in ms. Default 200.
- * @param {number|string} [options.idPrefix] - The prefix for id in details element. Works only if "createMultiple = false". Default is 1.
+ * @param {number|string} [options.idPrefix] - The id prefix for accordion.
  * @param {boolean} [options.createMultiple] - create multiple accordions or only one.
  */
 const createAccordion = ({
@@ -196,14 +226,15 @@ const createAccordion = ({
   detailsSelector = 'details',
   contentSelector = '.content',
   createMultiple = true,
-  idPrefix = 1,
+  idPrefix = 'accordion',
 }) => {
   if (createMultiple) {
     return generateMultipleAccordions(
       detailsSelector,
       contentSelector,
       duration,
-      easing
+      easing,
+      idPrefix
     );
   } else {
     return generateOneAccordion(
